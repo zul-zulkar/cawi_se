b08c3e32fb5278fc8c86b58f9af082d808829e2d
Zul <mzlkrnain1161@gmail.com>
2026-06-07 18:39:03 +0800

    fix: update DEFAULT_SCRIPT_URL to correct endpoint for script execution

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const DEFAULT_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxVyPaTpVmfxBvIPQc2u6kTMKWIZXT7LggpjGVCLV-pGHMRRQzL_zGEr1OjeuArB7OgDA/exec";
+const DEFAULT_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxX8J4X2yzUTzPADGTX6yDoMaO3pumbBEJYesnRONygaNDUlYTDy3k_pCDBzBXARCCyfw/exec";
 const SHEET_URL_KEY = 'cawi_script_url_override';
 function getScriptUrl() {
   return localStorage.getItem(SHEET_URL_KEY) || DEFAULT_SCRIPT_URL;
